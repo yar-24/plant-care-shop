5425ec7048fc1ad7a732c6202c53f18648bb9bea
rahmanqolbi <rahmanqolbi@gmail.com>
2022-11-03 17:30:18 +0700

    Menambahkan hero pada services

--- src/components/HeroPlantCare.js
+++ src/components/HeroPlantCare.js
@@ -1,24 +1,9 @@
 import React from "react";
-import { Container, Stack, Box, Typography, Button } from "@mui/material";
+import { Container, Stack, Box, Typography } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { colors, fonts } from "../utils/";
+import CustomButton from "./CustomButton";
+import { fonts } from "../utils/";
 import { PlantCareHero } from "../images/img";
-
-const ReadMoreButton = styled(Button)`
-  background-color: ${colors.secondary};
-  box-shadow: none;
-  text-transform: none;
-  align-self: flex-end;
-  margin: 16px 0;
-  padding: 16px 32px;
-  font-weight: 600;
-  font-family: ${fonts.inter};
-  &:hover {
-    background-color: ${colors.secondary};
-    background-image: linear-gradient(rgb(0 0 0/30%) 0 0);
-    box-shadow: none;
-  }
-`;
 
 const HeroImage = styled("img")`
   width: 100%;
@@ -48,9 +33,9 @@ const HeroPlantCare = () => {
             conditions for your plants, as long as they’re kept well watered. Here’s how to help
             them enjoy summer.
           </Typography>
-          <ReadMoreButton variant="contained" size="large">
+          <CustomButton size="large" sx={{ alignSelf: "flex-end" }}>
             Read More
-          </ReadMoreButton>
+          </CustomButton>
         </Stack>
         <Box sx={{ flex: 2 }}>
           <HeroImage src={PlantCareHero} alt="Plant" />
