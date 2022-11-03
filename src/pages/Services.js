d0246cddfd5bb7bec344b1d287a6ccc6118ef28c
Moch. Rofiqi <mrfiqi323@gmail.com>
2022-11-03 18:43:45 +0700

    responsif

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import BannerServices from '../components/BannerServices'
-import DetailServices from '../components/DetailServices'
 
 const Services = () => {
   return (
     <>
     <BannerServices/>
-    <DetailServices/>
     </>
   )
 }
