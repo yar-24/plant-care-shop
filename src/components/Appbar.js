448dfff541a9e135fa51f57cdb003e34c02aa3a4
Moch. Rofiqi <mrfiqi323@gmail.com>
2022-11-06 16:09:57 +0700

    appbar

--- src/components/Appbar.js
+++ src/components/Appbar.js
@@ -143,6 +143,10 @@ function DrawerAppBar(props) {
 }
 
 DrawerAppBar.propTypes = {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
     window: PropTypes.func,
 };
 
