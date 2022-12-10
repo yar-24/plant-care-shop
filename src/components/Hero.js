67fc8183de680587d7750aeaed7ea77bea42e0fc
Rahman Qolbi <rahmanqolbi@gmail.com>
2022-12-10 22:09:39 +0700

    responsive skeleton banner plant care

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,17 @@
-import React from "react";
-import styled from "styled-components";
-import { fonts, mobile } from "../utils/";
-import { PlantHero } from "../images/img";
-import Tombol from "./kecil/Tombol";
-import { Container, Stack, Box, Typography } from "@mui/material";
-import LocaleContext from "../contexts/LocaleContext";
-import { Link } from "react-router-dom";
+import { Box, Container, Stack, Typography } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import LocaleContext from '../contexts/LocaleContext';
+import { PlantHero } from '../images/img';
+import { fonts } from '../utils/';
+import Tombol from './kecil/Tombol';
 
 const Right = styled(Box)`
   display: flex;
   justify-content: end;
   position: relative;
   width: 50%;
-  /* ${mobile({ display: "none" })} */
 `;
 
 const Image = styled.img`
@@ -22,11 +21,10 @@ const Image = styled.img`
 
   @media (max-width: 1200px) {
     width: 100%;
-  } 
+  }
   @media (max-width: 900px) {
     width: 200%;
-  } 
-
+  }
 `;
 
 const Kotak = styled.div`
@@ -41,69 +39,77 @@ const Kotak = styled.div`
   );
   width: 200px;
   height: 100%;
-  max-height: 600px;
   position: absolute;
+  top: 0;
   right: 0;
-  /* ${mobile({ display: "none" })} */
 
   @media (max-width: 900px) {
     width: 150px;
-  }  
+  }
   @media (max-width: 600px) {
     width: 100px;
     height: 100%;
     max-height: 100%;
-  }  
+  }
 `;
 
 const Layout = styled.div`
   margin: 20px;
+  margin-left: 0;
 
   @media (max-width: 1200px) {
     margin: 0px auto;
-  }  
+  }
   @media (max-width: 900px) {
     margin: 85px auto;
-  }  
+  }
   @media (max-width: 600px) {
     margin: 20px auto;
-  }  
+  }
 `;
 
 const Hero = () => {
   const { locale } = React.useContext(LocaleContext);
 
   return (
-    <Container fixed>
-      <Stack direction="row" spacing={10}>
-      <Layout>
-        <Box my={{ xs: 5, md: 20 }} sx={{ flex: 5 }}>
-          <Typography
-            variant="h4"
-            component="h2"
-            sx={{ fontFamily: fonts.comfortaa, fontWeight: 700 }}
-            gutterBottom
-          >
-            {locale === 'id' ? 'Temukan tanaman terbaik untuk aura dan ruang anda. Lindungi napas anda.' : 'Discover the best plant for your aura and your space. Protect your breath.'}
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ fontFamily: fonts.inter, lineHeight: 2, mt: 2, mb: 3 }}
-            gutterBottom
-          >
-            {locale === 'id' ? 'Nafas membantu anda menemukan tanaman terbaik untuk ruang anda, mengirimkannya ke pintu anda dan membantu anda merawatnya.' : 'Breath helps you discover the best plants for your space, delivers them to your door and helps you look after them.'}
-          </Typography>
-          <Tombol component={Link} to={`/shop`}
-            label= {locale === 'id' ? 'Temukan Tanaman' : "Discover The Plants"}
-          />
-        </Box>
-        </Layout>
-        <Right>
-          <Image src={PlantHero} />
-        </Right>
-        <Kotak></Kotak>
-      </Stack>
-    </Container>
+    <Box position="relative" width="100%">
+      <Container fixed>
+        <Stack direction="row" spacing={10}>
+          <Layout>
+            <Box my={{ xs: 5, md: 20 }} sx={{ flex: 5 }}>
+              <Typography
+                variant="h4"
+                component="h2"
+                sx={{ fontFamily: fonts.comfortaa, fontWeight: 700 }}
+                gutterBottom>
+                {locale === 'id'
+                  ? 'Temukan tanaman terbaik untuk aura dan ruang anda. Lindungi napas anda.'
+                  : 'Discover the best plant for your aura and your space. Protect your breath.'}
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{ fontFamily: fonts.inter, lineHeight: 2, mt: 2, mb: 3 }}
+                gutterBottom>
+                {locale === 'id'
+                  ? 'Nafas membantu anda menemukan tanaman terbaik untuk ruang anda, mengirimkannya ke pintu anda dan membantu anda merawatnya.'
+                  : 'Breath helps you discover the best plants for your space, delivers them to your door and helps you look after them.'}
+              </Typography>
+              <Tombol
+                component={Link}
+                to={`/shop`}
+                label={
+                  locale === 'id' ? 'Temukan Tanaman' : 'Discover The Plants'
+                }
+              />
+            </Box>
+          </Layout>
+          <Right>
+            <Image src={PlantHero} />
+          </Right>
+        </Stack>
+      </Container>
+      <Kotak></Kotak>
+    </Box>
   );
 };
 
--- src/components/kecil/SkeletonBannerPlantCare.js
+++ src/components/kecil/SkeletonBannerPlantCare.js
@@ -3,13 +3,18 @@ import { Box, Container, Skeleton, Stack } from '@mui/material';
 
 const SkeletonBannerPlantCare = () => {
   return (
-    <Container>
-      <Stack my={10} sx={{ display: 'flex' }}>
-        <Box>
-          <Skeleton variant="rectangular" animation="wave" height={250} />
+    <Container fixed>
+      <Stack
+        direction={{ xs: 'column', md: 'row-reverse' }}
+        my={{ xs: 5, md: 10 }}
+        spacing={{ xs: 5, md: 10 }}>
+        <Box sx={{ flex: 2 }}>
+          <Skeleton variant="rectangular" animation="wave" height={350} />
         </Box>
-        <Box sx={{ marginTop: '20px' }}>
-          <Skeleton variant="text" animation="wave" width="50%" />
+        <Box mt={3} sx={{ flex: 3 }}>
+          <Skeleton variant="text" animation="wave" width="100%" />
+          <Skeleton variant="text" animation="wave" width="70%" />
+          <Skeleton variant="text" animation="wave" width="70%" />
           <Skeleton variant="text" animation="wave" width="50%" />
         </Box>
       </Stack>
